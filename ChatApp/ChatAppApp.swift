7cf41fdbf8219cd32c5a63a3b05fa29a6c0fa051
vuqt06 <vuqt06@gmail.com>
2022-05-27 14:56:28 +0700

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Vu Trinh on 5/27/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
