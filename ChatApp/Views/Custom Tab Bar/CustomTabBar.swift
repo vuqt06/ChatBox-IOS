815fda2f44d1209bb9eb24324d2b56fa2afaa9df
vuqt06 <vuqt06@gmail.com>
2022-05-28 15:32:04 +0700

    - Implement custom tab bar - Implement recycler view for tab bar buttons

---
+++ ChatApp/Views/Custom Tab Bar/CustomTabBar.swift
@@ -0,0 +1,62 @@
+//
+//  CustomTabBar.swift
+//  ChatApp
+//
+//  Created by Vu Trinh on 5/28/22.
+//
+
+import SwiftUI
+
+enum Tabs: Int {
+    case chats = 0
+    case contacts = 1
+}
+
+struct CustomTabBar: View {
+    @Binding var selectedTabs: Tabs
+    
+    var body: some View {
+        HStack (alignment: .center) {
+            Button {
+                // Switch to chats
+                selectedTabs = .chats
+            } label: {
+                TabBarButton(buttonText: "Chats", imageName: "bubble.left", isActive: selectedTabs == .chats)
+            }
+            .tint(Color("icons-secondary"))
+            
+            Button {
+                // Switch to new chat
+            } label: {
+                VStack(alignment: .center, spacing: 4) {
+                    Image(systemName: "plus.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 32, height: 32)
+                    Text("New Chat")
+                        .font(.tabBar)
+                }
+            }
+            .tint(Color("icons-primary"))
+
+            
+            
+            Button {
+                // Switch to contacts
+                selectedTabs = .contacts
+            } label: {
+                TabBarButton(buttonText: "Contacts", imageName: "person", isActive: selectedTabs == .contacts)
+            }
+            .tint(Color("icons-secondary"))
+
+
+        }
+        .frame(height: 82)
+    }
+}
+
+struct CustomTabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomTabBar(selectedTabs: .constant(.contacts))
+    }
+}
--- ChatApp/Views/RootView.swift
+++ ChatApp/Views/RootView.swift
@@ -8,18 +8,25 @@
 import SwiftUI
 
 struct RootView: View {
+    @State var selectedTab: Tabs = .contacts
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-    
-    init() {
-        for family in UIFont.familyNames {
-            for fontName in UIFont.fontNames(forFamilyName: family) {
-                print("--\(fontName)")
-            }
+        VStack {
+            Text("Hello, world!")
+                .padding()
+            
+            Spacer()
+            
+            CustomTabBar(selectedTabs: $selectedTab)
         }
     }
+    
+//    init() {
+//        for family in UIFont.familyNames {
+//            for fontName in UIFont.fontNames(forFamilyName: family) {
+//                print("--\(fontName)")
+//            }
+//        }
+//    }
 }
 
 struct RootView_Previews: PreviewProvider {
